b08159a373e7be535efb82a23ecd472eba8a43ef
Way45 <18602048869@163.com>
2018-08-07 20:49:50 +0800

    do some tunings and formats

--- client/main.js
+++ client/main.js
@@ -1,7 +1,8 @@
 import "./body.html";
 import "./bookmark_list.html";
 import "./bookmark_add.html";
+import "./bookmark.html"
 
-import "./body.js";
 import "../lib/collections.js";
 import "./bookmark_add.js";
+import "./bookmark_list.js";
