94a7e9decd9430914bcb7f55fc84e50878b428d9
Way45 <18602048869@163.com>
2018-08-07 21:21:11 +0800

    add 'modify markbook' function

--- client/bookmark.js
+++ client/bookmark.js
@@ -5,5 +5,44 @@ Template.bookmark.events({
             Bookmarks.remove({_id, id});
 
         }
+    },
+
+
+    'click span.edit': function (e, tpl) {
+        Session.set('currentEditingId', this._id);
+
+    },
+
+    'click btn-cancel': function (e, tpl) {
+        e.preventDefault();
+
+        Session.set('currentEditingId', null);
+
+    },
+
+
+    'submit form.form-edit': function (e, tpl) {
+        e.preventDefault();
+
+        var id = this._id;
+        var name = tpl.$('#bookmark_name_new').val();
+        var url = tpl.$('#bookmark_url_new').val();
+
+        Bookmarks.update({_id: id}, {$set: {name: name, url: url}}, function (err) {
+            if (!err) {
+                tpl.$('#bookmark_name_new').val('');
+                tpl.$('#bookmark_url_new').val('');
+
+                Session.set('currentEditingId', null);
+            }
+        });
     }
+
+});
+
+Template.bookmark.helpers({
+    isEditing: function () {
+        return Session.get('currentEditingId') == this._id;
+    }
+
 });
--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import "./bookmark_list.html";
 import "./bookmark_add.html";
 import "./bookmark.html"
 
+
 import "../lib/collections.js";
 import "./bookmark_add.js";
 import "./bookmark_list.js";
